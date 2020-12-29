e6a5eda7dcbffed541c1b15745e7573093559004
DESKTOP-97TO8KI\James <jamesmcneil42@hotmail.co.uk>
2020-12-29 21:34:34 +0000

    Added a super simple rotation script to make the cube look funky while distorted

---
+++ Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class RandomRotation : MonoBehaviour
+{
+    void Update()
+    {
+        float dt = Time.deltaTime;
+        transform.Rotate
+        (
+            dt * Random.Range(0.5f, 15.0f),
+            dt * Random.Range(0.5f, 15.0f),
+            dt * Random.Range(0.5f, 15.0f),
+            Space.Self
+        );
+    }
+}
